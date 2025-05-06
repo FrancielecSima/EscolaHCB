a616fee6f6187ddd9721fb2cab129dba200ccda7
Franciele Coss <francielecosssima@rede.ulbra.br>
2025-05-06 15:08:53 -0300

    Inicio do projeto

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Galeria de Fotos Escola HCB</title>
+  <link rel="stylesheet" href="src/style/index.css">
+</head>
+<body>
+  <main>
+  <section class="conteudo-principal">
+  <a href="index.php" class="logo">
+      <img src="./src/imagens/logo-horizon-hcb.png" alt="Logo HCB">
+    </a>
+    <img src="./src/imagens/registros-trajetorias.png" alt="Logo registros e trajetórias" class="trajetoria">
+    <img src="./src/imagens/seta.png" alt="Seta para avançar" class="seta-avancar">
+</section>
+
+  </main>
+
+</body>
+</html>
